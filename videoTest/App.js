d534ae35b5048afc4ed47a7b976027d23b4c81c3
remagart <jfmamjjasond418@gmail.com>
2018-11-08 08:32:44 +0800

    [add]能將影片在app上撥放，無control bar
    - import expo的video套件
    - 使用本地端的影片需用require
    - reference:
    - https://docs.expo.io/versions/latest/sdk/video
    - https://medium.com/front-end-hacking/how-to-play-video-with-react-native-and-expo-30523bfcb311
    
    20181108

--- videoTest/App.js
+++ videoTest/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Video} from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Video
+        source={require('./assets/ttt.mp4')}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={{ width: 300, height: 300 }}
+        />
       </View>
+ 
     );
+    
+    
+
   }
 }
 
