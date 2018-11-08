73d2fa3829baae6257f0ae6eaef9da64a46d0c80
remagart <jfmamjjasond418@gmail.com>
2018-11-08 10:29:53 +0800

    [add]新增control bar並將影片從遠端抓下
    - 使用youtube連結不行，要有附檔名mp4
    - 將control bar的設定寫在stylesheet中
    
    20181108

--- videoTest/App.js
+++ videoTest/App.js
@@ -8,15 +8,16 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Video
-        source={require('./assets/ttt.mp4')}
+        source={{uri:'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'}}
         rate={1.0}
         volume={1.0}
         isMuted={false}
         resizeMode="cover"
         shouldPlay
         isLooping
-        style={{ width: 300, height: 300 }}
+        style={{ width:300, height: 300 }}
         />
+        <View style={styles.controlBar}></View>
       </View>
  
     );
@@ -33,4 +34,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+controlBar:{
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  height: 60,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: 'rgba(0,0,0,0.5)',
+},
+
 });
+
