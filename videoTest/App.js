3776ab780ce0ffdf0ea669743cd24dfc8c05e7c5
remagart <jfmamjjasond418@gmail.com>
2018-11-08 11:44:06 +0800

    [add]新增播放暫停和靜音
    - 指令:npm install --save @expo/vector-icons
    - import expo的vector-icons套件新增元件的圖示
    - 使用state去做屬性和使用者互動
    - 使用state和setstate來調整狀態
    
    20181108

--- videoTest/App.js
+++ videoTest/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Video} from 'expo';
+import {MaterialIcons,Octions} from '@expo/vector-icons';
 
 export default class App extends React.Component {
+  state = {
+    mute: false,
+    shouldPlay: true,
+  }
+  handlePlayAndPause = () =>{
+    this.setState((prevState)=>({
+      shouldPlay: !prevState.shouldPlay,
+    }));
+  }
+  handleVolume = () =>{
+    this.setState(prevState => ({
+      mute: !prevState.mute,
+    }));
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Video
-        source={{uri:'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'}}
+        source={{uri:"http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}}
         rate={1.0}
         volume={1.0}
-        isMuted={false}
+        isMuted={this.state.mute}
         resizeMode="cover"
-        shouldPlay
+        shouldPlay={this.state.shouldPlay}
         isLooping
         style={{ width:300, height: 300 }}
         />
-        <View style={styles.controlBar}></View>
+        <View style={styles.controlBar}>
+          <MaterialIcons 
+            name = {this.state.mute ? "volume-mute" : "volume-up"}
+            size = {45}
+            color = "white"
+            onPress = {this.handleVolume}
+          />
+          <MaterialIcons 
+            name = {this.state.shouldPlay ? "pause" : "play-arrow"}
+            size = {45}
+            color = "white"
+            onPress = {this.handlePlayAndPause}
+          />
+        </View>
       </View>
  
     );
