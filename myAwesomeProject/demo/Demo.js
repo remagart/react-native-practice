199c5c6b21d4660699a553eba62308b6b953e8f9
remagart <jfmamjjasond418@gmail.com>
2018-11-12 16:11:38 +0800

    [myAwesomeProject][Add]練習Text屬性和debug
    - onLayout可用來抓頁面長寬資訊
    - numberOfLines用來限制行數
    - onPress用於點擊事件
    - onLongPress用於長點擊事件
    ----------debug------------
    - 搖動手機打開menu並enable Remote JS Debugging
    - 即可在Chrome看到debug的頁面
    - 按Ctrl Shift j 打開console即可看console.log()
    
    20181112

--- myAwesomeProject/demo/Demo.js
+++ myAwesomeProject/demo/Demo.js
@@ -5,12 +5,34 @@ export default class Demo extends Component {
   render() {
     return (
       <View>
-        <Text style={mystyle}> Hello, this is demo! </Text>
-        <Text>I'm Richerd</Text>
+        <Text style={mystyle}
+        numberOfLines = {2}
+        onLayout = {({nativeEvent: {layout: {x, y, width, height}}})=>{
+          //搖動手機打開menu並enable Remote JS Debugging
+          //即可在Chrome看到debug的頁面
+          //按Ctrl Shift j 打開console即可看console.log()
+          console.log("zzzz");
+          console.log(x, y, width, height);
+        }}
+        > Hello, this is demo!Hello, this is demo!Hello, this is demo!Hello, this is demo!Hello, this is demo!Hello, this is demo! </Text>
+        <Text
+        onPress = {function(){
+          Alert.alert("So far so good");
+        }}
+        >How are you？</Text>
+        <Text
+          style = {mymargin}
+          onLongPress={function(){
+            Alert.alert("My name is Richard");
+          }}
+        >Please press long time to see.</Text>
         <Button 
           title = "Demo"
           color = "#42f4ce"
-          onPress = {function(){Alert.alert("this is demo in other file")}}
+          onPress = {function(){
+            Alert.alert("this is demo in other file");
+            console.log("hihih");
+          }}
         />
       </View>
     );
@@ -27,5 +49,8 @@ var mystyle = {
     height:5
   },
   textShadowColor: "red",
-  
 };
+
+var mymargin = {
+  top: 100,
+}
