3b64714d41bb57dfe62a527d3193491d220e0623
remagart <jfmamjjasond418@gmail.com>
2018-11-12 11:17:31 +0800

    [myAwesomeProject][Add]測試按鈕和Alert
    - import Button和Alert
    - 於view中新增Button事件處理，並跳出Alert
    
    20181112

--- myAwesomeProject/App.js
+++ myAwesomeProject/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,Button,Alert} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,8 +22,13 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          title = "hello"
+          color = "green"
+          onPress = {function(){Alert.alert("you press me!!")}}
+        />
       </View>
     );
   }
