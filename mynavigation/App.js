0f73eabaa5614772c70ba22e8ef2e047910dbb2a
remagart <jfmamjjasond418@gmail.com>
2018-11-15 14:55:57 +0800

    [mynavigation2][Install]安裝react-navigation
    - 因為mynavigation一直有error code 500的問題
    - 所以再建一個專案mynavigation2
    - 安裝使用yarn
    - 安裝react-navigation
    - 指令：yarn add react-navigation
    - ===========================================
    - 使用createStackNavigator設定路由
    - 設定了兩個路由:Home和Detail
    - 設定button進行頁面互傳
    
    20181115

--- mynavigation/App.js
+++ mynavigation/App.js
@@ -7,8 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator} from 'react-navigation'
+import {Platform, StyleSheet, Text, View,Button} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
 
 // const instructions = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,26 +19,38 @@ import {createStackNavigator} from 'react-navigation'
 
 type Props = {};
 export default class App extends Component<Props> {
+  console.log("ccc");
   render() {
     return <RootStack />;
   }
 }
 
 class HomeScreen extends Component{
+  console.log("aaa");
   render(){
     return(
         <View>
           <Text>Welcome to Home</Text>
+          <Button 
+            title = "go to detail"
+            onPress = {
+              () => this.props.navigation.navigate('Detail')}
+          />
         </View>
     );
   }
 }
 
 class DetailScreen extends Component{
+  console.log("bbb");
   render(){
     return(
       <View>
         <Text>this is detail</Text>
+        <Button 
+            title = "go to detail"
+            onPress = {() => this.props.navigation.navigate('Detail')}
+          />
       </View>
     );
   }
@@ -50,6 +62,6 @@ const RootStack = createStackNavigator(
     Detail: DetailScreen,
   },
   {
-    initialRouteName:'Detail',
+    initialRouteName:'Home',
   },
 )
