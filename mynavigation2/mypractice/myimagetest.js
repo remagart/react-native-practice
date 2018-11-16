c9feb648b0036200d7e97e01c89809f6038f870f
remagart <jfmamjjasond418@gmail.com>
2018-11-16 16:04:34 +0800

    [mynavigation2][Add]新增圖片樣式表
    - 使用Image要連遠端圖片，需用HTTPS的source
    - 因為IOS平台的限制
    - 遠端圖片要設定長寬，不然不顯示
    
    20181116

--- mynavigation2/mypractice/myimagetest.js
+++ mynavigation2/mypractice/myimagetest.js
@@ -5,10 +5,26 @@ export default class myimagetest extends Component {
   render() {
     return (
       <View>
-        <Image source={require('./icon.png')}/>
-        <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR18NojE7FZxyzeAHvoE5zqlBKxNyp9jak_G_h-DYM7uT4F_Vq3'}}/>
-        {/* <Text>HIHI</Text> */}
+        <Text style = {styles.mytxtstyle}>這是 local img</Text>
+        <Image style={styles.mystyle} source={require('./icon.png')}/>
+        <Text style = {styles.mytxtstyle}>This is https img</Text>
+        <Image style={styles.mystyle} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR18NojE7FZxyzeAHvoE5zqlBKxNyp9jak_G_h-DYM7uT4F_Vq3'}}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    mystyle:{
+        width: 200,
+        height: 200,
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+    mytxtstyle:{
+        fontSize:20,
+        color:"blue",
+        fontWeight:"bold",
+        fontStyle:"italic",
+    },
+});
