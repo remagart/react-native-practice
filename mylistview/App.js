2f20a27175926fd8eb24386d525ac20537305f98
remagart <jfmamjjasond418@gmail.com>
2018-11-19 16:40:59 +0800

    [mylistview][Install]安裝react-navigation v3
    - 因為debug react-navigation v2到v3，所以將listview相關砍了
    - 指令:
    - yarn add react-navigation
    - yarn add react-native-gesture-handler
    - react-native link
    - ===============================
    - 需import createAppContainer,createStackNavigator
    - const AppContainer = createAppContainer(自己的Route);
    - 承上，自己的Route需先定義好，因JS是直譯式
    - 以下需在AppContainer宣告完後才能寫
    export default class App extends Component<Props> {
      render(){
        return (
          <AppContainer />
        );
      }
    }
    
    20181119

--- mylistview/App.js
+++ mylistview/App.js
@@ -7,102 +7,45 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,ListView} from 'react-native';
+import {Platform, StyleSheet, Text, View,Button} from 'react-native';
+import {createAppContainer,createStackNavigator } from 'react-navigation';
+
+
 
 
 type Props = {};
-export default class App extends Component<Props> {
-  constructor(props){
-    super(props);
-    const ds = new ListView.DataSource({
-      rowHasChanged : (r1,r2) => r1!==r2
-    });
 
-    this.state = {
-      dataSource: ds.cloneWithRows(['row1',
-                                    'row2',
-                                    'row3',
-                                    'row1',
-                                    'row2',
-                                    'row3',
-                                    'row1',
-                                    'row2',
-                                    'row3',
-                                    'row1',
-                                    'row2',
-                                    'row3',
-                                    'row1',
-                                    'row2',
-                                    'row3']),
-    }
 
-    // this.dataSource = new ListView.DataSource({
-    //   rowHasChanged:(r1,r2) => r1!==r2,
-    //   sectionHeaderHasChanged:(s1,s2)=>s1!==s2,
-    //   getRowData:(data,sID,rID)=>{
-    //       console.log(data,sID,rID);
-    //       return data[sID][rID];
-    //   },
-    //   getSectionHeaderData:(data,sID)=>{
-    //     return "我的分區"+sID;
-    //   },
-    // });
-    // this.dataSource = this.dataSource.cloneWithRows([
-    //   "資料A","資料B",
-    //   "資料C","資料D",
-    //   "資料E","資料F",
-    // ]);
+class MyHomeScreen extends Component{
+  render(){
+    return(
+      <Text>HIHI</Text>
+    );
   }
-
-    render(){
-      return(
-        <ListView 
-          dataSource={this.state.dataSource}
-          renderRow={(rowdata)=> <Text style = {styles.myrow}>{rowdata}</Text>}
-        />
-      );
-    };
-
-  // render() {
-  //   return (
-  //     <ListView 
-  //       dataSource={this.dataSource}
-  //       renderRow={(rowData)=>{
-  //         return (
-  //         <Text
-  //           style = {styles.myrow}
-  //         >{rowData}</Text>
-  //         );
-  //       }}
-  //       renderSectionHeader={(headerData,sID)=>{
-  //         return(
-  //           <Text
-  //             style = {styles.mysec}
-  //             onPress = {()=>{
-  //               let rows = this.dataSource.getRowCount();
-  //               let secs = this.dataSource.getRowAndSectionCount();
-  //               console.log(rows,secs);
-  //               }
-  //             }
-  //           >{headerData}</Text>
-  //         );
-  //       }}
-  //     />
-  //   ) 
-  // };
 }
 
-const styles = StyleSheet.create({
-  myrow:{
-    lineHeight:50,
-    marginTop:10,
-    backgroundColor:"gray",
-    textAlign: "center",
+class MyListScreen extends Component{
+  render(){
+    return(
+      <Text>HIHhuhuI</Text>
+    );
+  }
+}
+const MyRoute = createStackNavigator(
+  {
+    MyHome: MyHomeScreen,
+    MyList: MyListScreen,
   },
-  mysec:{
-    lineHeight:30,
-    // marginTop:30,
-    backgroundColor:"green",
-    textAlign:"center",
+  {
+    initialRouteName: 'MyHome',
   },
-});
+);
+
+const AppContainer = createAppContainer(MyRoute);
+export default class App extends Component<Props> {
+  render(){
+    return (
+      <AppContainer />
+    );
+  }
+}
