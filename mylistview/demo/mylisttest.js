e197a64d90d3c6fb3a15e1f19e2a82a269650c44
remagart <jfmamjjasond418@gmail.com>
2018-11-20 16:32:26 +0800

    [mylistview][Add]嘗試用其他JSON
    - 發現responsedata後面要接JSON資料的目錄
    - Ex. {JSON}/{result}/[records]/{}{}{}{}...
    - 那就要dataSource.cloneWithRows(responsedata.result.records)
    - ===================================
    - fetch().then().then().catch()
    - fetch的用法和JS的promise類似，用於非同步工作的實現
    - fetch(JSON的網址)，成功就會往下執行then
    - then((response)=>response.json())，成功就會往下執行then
    - then((responsedata)=>{...})，responsedata是JSON的根目錄
    - 承上，所以要在這函式中將資料指向JSON資料的目錄
    - catch中放fetch錯誤要做的事情
    - ====================================
    - response.json()只能做一次，不然會錯誤Already read
    - (response)=>response.json() 可寫成如下
    - (response)=>{ return response.json(); }
    
    20181120

--- mylistview/demo/mylisttest.js
+++ mylistview/demo/mylisttest.js
@@ -4,6 +4,7 @@ import { Text, View,ListView,Image,StyleSheet } from 'react-native'
 export default class mylisttest extends Component {
     constructor(props){
         super(props);
+        console.log("zzzz");
         this.state = {
             dataSource: new ListView.DataSource({
                 rowHasChanged: (r1,r2) => r1 !== r2
@@ -11,19 +12,30 @@ export default class mylisttest extends Component {
         }
     }
     componentDidMount(){
+        
+        console.log("hiddddd");
         this.myfetchData();
     }
 
     myfetchData(){
         //fetch().then().then().done();
-        fetch("https://api.douban.com/v2/movie/in_theaters")
-        .then((response)=>response.json())
+        //fetch("https://api.douban.com/v2/movie/in_theaters")
+        console.log("hihihi");
+        fetch("https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=F")
+        .then((response)=>{
+            //不能call json兩次
+            //console.log(response.json());
+             return response.json();
+        })
         .then((responsedata)=>{
+            //console.log(==分隔線==);
+            console.log(responsedata);
             this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(responsedata.subjects),
+                //dataSource: this.state.dataSource.cloneWithRows(responsedata.subjects),
+                dataSource: this.state.dataSource.cloneWithRows(responsedata),
             });
         })
-        .done();
+        .catch((err)=>{console.log("aaaa");});
     }
 
     render() {
@@ -41,12 +53,16 @@ export default class mylisttest extends Component {
         return(
             <View>
                 <Image style = {styles.myimgstyle}
-                    source={{uri: rowData.images.large}}
+                    source={{uri: rowData.representImage}}
                 />
                 <View>
-                    <Text>{rowData.title}</Text>
+                    {/* <Text>{rowData.title}</Text>
                     <Text>年份: {rowData.year}</Text>
-                    <Text>評分: {rowData.rating.max}</Text>
+                    <Text>評分: {rowData.rating.max}</Text> */}
+                    <Text>{rowData.name}</Text>
+                    <Text>地址: {rowData.address}</Text>
+                    <Text>Size: {rowData.size}</Text>
+                    <Text>描述: {rowData.intro}</Text>
                 </View>
             </View>
         );
