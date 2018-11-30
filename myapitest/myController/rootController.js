3318e3b99d2f664e4a19fd939b5716e1f2ab2f55
remagart <jfmamjjasond418@gmail.com>
2018-11-30 16:10:04 +0800

    [myapitest][Add] 新增status bar
    - 手機最上面電量那排
    - IOS和android都有
    - 記得上一層View要設flex:1
    - 不然會沒畫面
    
    20181130

--- myapitest/myController/rootController.js
+++ myapitest/myController/rootController.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,StatusBar,StyleSheet } from 'react-native';
 import RootNavigate from './../mynavigate/rootnavigate';
 
 export default class rootController extends Component {
   render() {
     return (
-      <RootNavigate  />
+      <View style={mystyles.container}>
+        <StatusBar
+            backgroundColor = "red"
+            barStyle = "light-content"
+        />
+        <RootNavigate  />
+      </View>
     )
   }
 }
+
+const mystyles = StyleSheet.create({
+  container:{
+      flex: 1,
+      backgroundColor: "red",
+  },
+});
