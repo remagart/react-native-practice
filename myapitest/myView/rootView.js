a44c1db156de6043b32723f281468971004dcaaa
remagart <jfmamjjasond418@gmail.com>
2018-11-30 23:01:37 +0800

    [myapitest][Add] 發現0.19.1和1.0.0-beta7差別
    - 所以還是要看板號，有時差一版差很多
    - 練習Avator
    - icon在右邊和loading在右邊得以解決
    
    20181130

--- myapitest/myView/rootView.js
+++ myapitest/myView/rootView.js
@@ -1,39 +1,50 @@
 import React, { Component } from 'react';
 import { Text, View,StyleSheet } from 'react-native';
-import {Button,Icon} from 'react-native-elements';
+import {Button,Icon,Avatar} from 'react-native-elements';
 
 export default class rooView extends Component {
   render() {
     return (
       <View style = {mystyle.container}>
         <Text> Hello this is rootView </Text>
-        <Button
-          title = "magicboy"
-          color = "gray"
-          fontSize = {20}
-          size = {40}
-          fontWeight = "bold"
-          backgroundColor = "blue"
-          //react-native-element預設是MaterialIcons
-          // type (defaults to material, options are material-community, zocial,
-          //  font-awesome, octicon, ionicon, foundation, evilicon, simple-line-icon, 
-          // feather or entypo)
-          // https://oblador.github.io/react-native-vector-icons/
-          icon = {{
-            name: "aircraft-take-off",
-            type: "entypo",
-            size: 30
-          }}
-          borderRadius = {60}
-          
-        />
-
-        <Button 
-          title = "this is loading"
-          loading
-        />
-
-        
+        <View style = {mystyle.mymiddle}>
+           <Avatar
+              large
+              title = "RR"
+              rounded
+          />
+        </View>
+       
+        <View style = {mystyle.mybtn}>
+            <Button
+              title = "magicboy"
+              color = "gray"
+              fontSize = {20}
+              size = {40}
+              fontWeight = "bold"
+              backgroundColor = "blue"
+              //react-native-element預設是MaterialIcons
+              // type (defaults to material, options are material-community, zocial,
+              //  font-awesome, octicon, ionicon, foundation, evilicon, simple-line-icon, 
+              // feather or entypo)
+              // https://oblador.github.io/react-native-vector-icons/
+              rightIcon = {{
+                name: "aircraft-take-off",
+                type: "entypo",
+                size: 30
+              }}
+              
+              borderRadius = {60}
+              
+            />
+        </View>
+        <View style={mystyle.mybtn}>
+            <Button 
+              title = "this is loading"
+              loadingRight
+              loading
+            />
+        </View>
         
       </View>
     )
@@ -44,4 +55,11 @@ const mystyle = StyleSheet.create({
     container:{
         flex: 1,
     },
+    mymiddle:{
+      alignItems: 'center',
+      margin: 20,
+    },
+    mybtn:{
+      margin:10,
+    }
 });
