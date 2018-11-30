ab46d1ddbd262b0a3605a48559455d8c682c71a0
remagart <jfmamjjasond418@gmail.com>
2018-11-30 22:15:03 +0800

    [myapitest][Add]練習Button(react-native-element)
    - 新增API:react-native-vector-icons
    - 練習Button (react-native-element)
    - 包含icon和loading
    
    20181130

--- myapitest/myView/rootView.js
+++ myapitest/myView/rootView.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,StyleSheet } from 'react-native';
+import {Button,Icon} from 'react-native-elements';
 
 export default class rooView extends Component {
   render() {
     return (
-      <View>
+      <View style = {mystyle.container}>
         <Text> Hello this is rootView </Text>
+        <Button
+          title = "magicboy"
+          color = "gray"
+          fontSize = {20}
+          size = {40}
+          fontWeight = "bold"
+          backgroundColor = "blue"
+          //react-native-element預設是MaterialIcons
+          // type (defaults to material, options are material-community, zocial,
+          //  font-awesome, octicon, ionicon, foundation, evilicon, simple-line-icon, 
+          // feather or entypo)
+          // https://oblador.github.io/react-native-vector-icons/
+          icon = {{
+            name: "aircraft-take-off",
+            type: "entypo",
+            size: 30
+          }}
+          borderRadius = {60}
+          
+        />
+
+        <Button 
+          title = "this is loading"
+          loading
+        />
+
+        
+        
       </View>
     )
   }
 }
+
+const mystyle = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+});
