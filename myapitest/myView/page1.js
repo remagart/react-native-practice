1671c3fcd7cca477db68f4171ea7e39ca09e3785
remagart <jfmamjjasond418@gmail.com>
2018-12-03 17:33:35 +0800

    [myapitest][Install/Add] 練習時間
    - yarn add moment@2.22.2
    - 關鍵在format()要放什麼
    
    20181203

--- myapitest/myView/page1.js
+++ myapitest/myView/page1.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View,StyleSheet } from 'react-native';
+import moment from 'moment';
 
 export default class page1 extends Component {
+
+  mytime = require('moment');
+
+  constructor(props){
+    super(props);
+
+  }
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style = {mystyle.mycontainer}>
+        <Text> 現在是 {this.mytime().format('YYYY-MM-DD HH:mm:ss')} </Text>
+        <Text>
+            今天星期 {this.mytime().format('D')}
+        </Text>
       </View>
     )
   }
 }
+
+const mystyle = StyleSheet.create({
+  mycontainer:{
+    flex:1,
+    alignItems:'center',
+  },
+});
