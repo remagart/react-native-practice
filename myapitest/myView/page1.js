f5f31762d804071ecc59b8881dcfa97b19d78ce5
remagart <jfmamjjasond418@gmail.com>
2018-12-04 11:32:11 +0800

    [myapitest][Add] 測試moment()
    - 找到locale code好用網站
    - https://www.science.co.il/language/Locale-codes.php
    - 下一步:需看看要如何記錄時間
    
    20181204

--- myapitest/myView/page1.js
+++ myapitest/myView/page1.js
@@ -11,12 +11,29 @@ export default class page1 extends Component {
 
   }
 
+  getTimelineFormatString(year, month) {
+    return moment({y: year, M: month - 1}).locale('zh-tw').format('MMM, YYYY');
+  }
+
+  getCompCourseFormatString(millisecond) {
+    return moment(millisecond).locale('zh-tw').format('YYYY/MM/DD HH:mm');
+  }
+
   render() {
     return (
       <View style = {mystyle.mycontainer}>
         <Text> 現在是 {this.mytime().format('YYYY-MM-DD HH:mm:ss')} </Text>
         <Text>
             今天星期 {this.mytime().format('D')}
+        </Text>
+        <Text>
+            全部是 {this.mytime().format()}
+        </Text>
+        <Text>
+           測試傳入年月 {this.getTimelineFormatString(2018,4)}
+        </Text>
+        <Text>
+           測試傳入millisecond {this.getCompCourseFormatString(99999999999)}
         </Text>
       </View>
     )
