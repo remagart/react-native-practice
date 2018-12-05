5cff512dc69abe2489347b3ed05caccbdb8dd3c5
remagart <jfmamjjasond418@gmail.com>
2018-12-05 15:23:07 +0800

    [myapitest][Add] 練習pop out的對話
    - isVisible控制要不要顯示，和dialog一樣
    - onClose控制要怎麼關掉
    - fromView使對話框有個箭頭
    - placement控制對話框的位置，也可選擇auto
    - showBackground控制整個螢幕要不要有背景
    - popoverStyle可在裡面放StyleSheet控制對話框樣式
    - arrowStyle可在裡面放StyleSheet控制箭頭樣式
    - =========================================
    - 於其他元件設定ref
    - ref = {(ref)=>{
    this.mytouchable = ref;
            }}
    - 於popover元件即可用
    - fromView = {this.mytouchable}
    
    20181205

--- myapitest/myView/Page4.js
+++ myapitest/myView/Page4.js
@@ -1,12 +1,88 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View,StyleSheet,TouchableHighlight } from 'react-native';
+import MyPopOver from 'react-native-popover-view';
 
 export default class Page4 extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+        isShow: false,
+      }
+  }
+
+  onHandlepopopen = () =>{
+    this.setState({
+      isShow: true,
+    });
+  }
+
+  onHandlepopclose = () =>{
+    this.setState({
+      isShow: false,
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style = {mystyle.container}>
+        <View style = {mystyle.myheader}>
+          <TouchableHighlight 
+            ref = {(ref)=>{
+                this.mytouchable = ref;
+            }}
+            //ref = {ref => this.mytouchable = ref}
+            onPress = {this.onHandlepopopen}
+          >
+            <Text>hihihi</Text>
+          </TouchableHighlight>
+          <MyPopOver
+             isVisible = {this.state.isShow}
+             onClose = {this.onHandlepopclose}
+             fromView = {this.mytouchable}
+             placement = "bottom"
+             showBackground = {false}
+             popoverStyle = {mystyle.mypopstyle}
+             arrowStyle = {mystyle.myarror}
+          >
+            <Text>hello MyPopOver</Text>
+          </MyPopOver>
+        </View>
+        <View style = {mystyle.mybody}>
+
+        </View>
       </View>
     )
   }
 }
+
+const mystyle = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  myheader:{
+    flex: 0.3,
+    backgroundColor: "#FFE194",
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  mybody:{
+    flex: 0.7,
+    backgroundColor: "#99E0FF",
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  mypopstyle:{
+    backgroundColor: "#6B7BDC",
+    borderRadius: 60,
+    width: 150,
+    height: 80,
+    paddingLeft: 30,
+    margin: 10,
+  },
+  myarror:{
+    width: 10,
+    height: 10,
+  }
+
+});
