55e2d672a1c2c5bf172274930d155ec755759b9e
remagart <jfmamjjasond418@gmail.com>
2018-12-05 09:44:19 +0800

    [myapitest][Add] 練習checkBox切換語系
    - 搭配著react-native-elements的切換按鈕並切換語言顯示
    - 不知道為何沒辦法使用getLanguage()和getInterfaceLanguage()
    
    20181205

--- myapitest/myView/Page3.js
+++ myapitest/myView/Page3.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,Alert,StyleSheet } from 'react-native'
+import MyLanguage from 'react-localization';
+import LocalizedStrings from 'react-localization';
+import {Button,CheckBox} from 'react-native-elements';
 
 export default class Page3 extends Component {
+    mytxt = new LocalizedStrings({
+        'zh-tw':{
+            how:"淡淡的哀傷",
+            lang: "中文",
+        },
+        'en':{
+            how:"this is egg crying",
+            lang: "英文",
+        },
+    });
+
+  constructor(props){
+      super(props);
+      this.state = {
+          //true means English
+          //false means Mandarin
+          isEnglish: true,
+          mycolor: "red",
+      }
+  }
+
   render() {
+    //this.mytxt.setLanguage('zh-tw');
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style = {mystyle.container}>
+        <View style = {mystyle.myheader}>
+            <CheckBox
+                title = {this.mytxt.lang}
+                checked = {this.state.isEnglish}
+                checkedColor = {this.state.mycolor}
+                onPress = {()=>{
+                    if(this.state.isEnglish === true){
+                        this.mytxt.setLanguage('zh-tw');
+                    }
+                    else{
+                        this.mytxt.setLanguage('en');
+                    }
+                    this.setState({
+                        isEnglish: !this.state.isEnglish,
+                     });
+                }}
+            />
+        </View>
+        <View style = {mystyle.mybody}>
+            <Text>
+                {this.mytxt.how}
+            </Text>
+        </View>
       </View>
     )
   }
 }
+
+const mystyle = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection: 'column',
+    },
+    myheader:{
+        flex: 0.5,
+        backgroundColor: "#4286f4",
+    },
+    mybody:{
+        flex: 0.5,
+        backgroundColor:"#fdddff", 
+    },
+});
+
