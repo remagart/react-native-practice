197745d9d73e2b4331e7bd0e109d6e6a29bf7a47
remagart <jfmamjjasond418@gmail.com>
2018-12-09 00:42:42 +0800

    [mypromise_ref][Add] 實作出Promise
    - 製作出自己的Promise
    - 用另一隻function呼叫Promise並做then
    - ========================================
    - let a = parseInt(Math.random()*2);
    - 承上，a會是Number型態，亂數出來的結果會有0或1
    - onPress = {this.mytest2.bind(this)}
    - ========================================
    - 承上，ES6後不會自動bind函數，所以要自己加
    - 不然不能在函式中使用this.setstate
    
    20181209

--- mypromise_ref/src/mypromise.js
+++ mypromise_ref/src/mypromise.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,Alert } from 'react-native';
+import {Button} from 'react-native-elements';
 
 export default class mypromise extends Component {
+  constructor(props){
+    super(props);
+  }
+  mytest(someone){
+      let a = parseInt(Math.random()*2);
+      let result;
+      result = new Promise((resolve,reject) => {
+        if(a == 0){
+          resolve(`${someone} 成功了!`);
+        }
+        else{
+          reject(`${someone} 失敗了!!`);
+        }
+      })
+      return result;
+  }
+
+  mytest2(){
+    this.mytest("apple")
+    .then((data)=>{
+      Alert.alert(data);
+    })
+    .catch((err)=>{
+      Alert.alert(err);
+    });
+  }
+
   render() {
     return (
       <View>
         <Text> textInComponent </Text>
+        <Button
+          title = "press me"
+          onPress = {this.mytest2.bind(this)}
+        />
+    
       </View>
     )
   }
