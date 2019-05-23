51c4450a5a3d786fe6a7b9ff0b1c2639bb914240
remagart <jfmamjjasond418@gmail.com>
2019-05-23 10:31:16 +0800

    [Add] build stage的apk 和 進度條
    - 在buildTypes中新增了額外兩個
    - debug 和releaseStage
    - 可以利用Android Studio產生releaseStage的apk
    - 並且與對應的CodePush key對應到
    - 值得注意的是，要加入以下，不然不給build
    - matchingFallbacks = ['debug', 'release']
    -
    - 產生進度轉圈圈
    
    20190522

--- myCodePush/App.js
+++ myCodePush/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,Button} from 'react-native';
-import upgrade from "./src/upgrade";
+import Upgrade from "./src/upgrade";
+import CodePush from "react-native-code-push";
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -16,10 +17,17 @@ export default class App extends Component<Props> {
     this.setState({
       version: 2,
     });
+    // CodePush.disallowRestart();
   }
 
   componentDidMount(){
-    upgrade.myfunc();
+    CodePush.notifyAppReady();
+    // let up = new Upgrade();
+    // up.checkforUpdate();
+  }
+
+  componentWillUnmount(){
+    // CodePush.allowRestart();
   }
 
   onClickedButton = () => {
@@ -28,14 +36,26 @@ export default class App extends Component<Props> {
     });
   }
 
+  callback = () => {
+    console.log("callback");
+  }
+
   render() {
     let packages = require("./package.json");
     return (
       <View style={styles.container}>
         <Text>This is practice of CodePush</Text>
         <Text>version: {this.state.version}</Text>
-        <Button title="press me" onPress= {this.onClickedButton} />
-        
+        <Button title="Please press me!!!" onPress= {this.onClickedButton} />
+        <Text>this is new one</Text>
+        <Text>this is new two</Text>
+        <Text>this is new three</Text>
+        <Text>this is new four!!!!!!!!</Text>
+        <Text>this is new seven</Text>
+        <Text>this is new eight</Text>
+
+        <Upgrade mycallback={this.callback} />
+
         <Text>packagename: {packages.name}</Text>
         <Text>packagejson: {packages.version}</Text>
       </View>
--- myCodePush/src/upgrade.js
+++ myCodePush/src/upgrade.js
@@ -1,19 +1,102 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,Button,Alert,ProgressBarAndroid} from 'react-native';
 import CodePush from "react-native-code-push";
 
-export default myupgrade = {
-    myfunc(){
+// prod
+// const deployKey = "bgCyxUhmgoEvjdJsG1xD0M2k0tsP23366673-b448-461f-a007-5e75244bcb41";
+// stage
+const deployKey = "NIWhXwwQjZQaRU-FYuUixGZFcHTp23366673-b448-461f-a007-5e75244bcb41";
+
+export default class myupgrade extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            showProgress: false,
+            inProgress: 0,
+        }
+    }
+
+    componentDidMount(){
+        this.checkforUpdate();
+    }
+
+    shouldComponentUpdate(nextProps,nextState){
+        this.props.mycallback();
+    }
+
+    checkforUpdate = () => {
+        CodePush
+            .checkForUpdate(deployKey)
+            .then((update)=>{
+                console.log(deployKey);
+                if(!update){
+                    // Alert.alert("提示","已經最新版本",[
+                    //     {
+                    //         text: "OK",onPress:()=>{}
+                    //     }
+                    // ]);
+                    console.log("Already up-to-date");
+                }
+                else{
+                    this.myfunc();
+                }
+            });
+
+    }
+
+    myfunc = () => {
         console.log("updatetetete");
         CodePush.sync({
-            updateDialog: {
-                appendReleaseDescription: true,
-                descriptionPrefix:'\n\n更新内容：\n',
-                title:'更新',
-                mandatoryUpdateMessage:'',
-                mandatoryContinueButtonLabel:'更新',
-            },
+            deploymentKey: deployKey,
+            // updateDialog: {
+            //     appendReleaseDescription: true,
+            //     descriptionPrefix:'\n\n更新内容：\n',
+            //     title:'更新',
+            //     mandatoryUpdateMessage:'這是更新',
+            //     mandatoryContinueButtonLabel:'更新',
+            // },
             mandatoryInstallMode:CodePush.InstallMode.IMMEDIATE,
-            deploymentKey: "bgCyxUhmgoEvjdJsG1xD0M2k0tsP23366673-b448-461f-a007-5e75244bcb41",
+        },(status)=>{
+            switch(status){
+                case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+                    this.setState({showProgress: true});
+                    break;
+                case CodePush.SyncStatus.INSTALLING_UPDATE:
+                    this.setState({showProgress: true});
+                    break;
+                case CodePush.SyncStatus.UPDATE_INSTALLED:
+                    this.setState({showProgress: false});
+                    break;
+                default:
+            }
+        },(receivedBytes_Obj)=>{
+            this.setState({inProgress: (receivedBytes_Obj.receivedBytes / receivedBytes_Obj.totalBytes).toFixed(2)})
+            console.log(`receivedBytes:`);
+            console.log(receivedBytes_Obj.receivedBytes);
+            console.log(`totalBytes: ${receivedBytes_Obj.totalBytes}`);
         });
+    }
+    render(){
+        console.log(this.state.inProgress);
+        console.log(typeof this.state.inProgress);
+        let test = parseFloat((this.state.inProgress));
+        return(
+            <View>
+                <Text>hihi</Text>
+                {
+                    (this.state.showProgress) ?
+                    <ProgressBarAndroid
+                        // styleAttr='Horizontal'
+                        indeterminate = {false}
+                        color= "red"
+                        progress = {test}
+                    />
+                    : null
+                }
+                
+            </View>
+        );
     }
 
 };
