53206204ea9de8e337b5a734d6bf6265e3350df0
remagart <jfmamjjasond418@gmail.com>
2019-07-15 14:57:03 +0800

    [myTestRedux] redux連結子component - Provider / connect / createStore的使用
    20190715

--- myTestRedux/App.js
+++ myTestRedux/App.js
@@ -8,42 +8,33 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import TestComponent from "./testComponent"
+import {createStore} from "redux"
+import {Provider,connect} from "react-redux"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const counterReducer = (state = {counter:50}) =>{
+  return state;
+};
+const store = createStore(counterReducer);
 
-type Props = {};
-export default class App extends Component<Props> {
+const mapStateToProps = state => {
+  return {
+    counter: state.counter
+  };
+}
+
+const CounterContainer = connect(mapStateToProps)(TestComponent);
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Text>Welcome to React Native!</Text>
+        <Provider store={store}>
+          <CounterContainer />
+        </Provider>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
