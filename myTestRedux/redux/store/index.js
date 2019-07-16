c2449311c763175e4e66a7a4c0393ba9c27358ea
remagart <jfmamjjasond418@gmail.com>
2019-07-16 16:51:36 +0800

    [myTestRedux] react-native debugger
    20190716

--- myTestRedux/redux/store/index.js
+++ myTestRedux/redux/store/index.js
@@ -1,7 +1,11 @@
-import {createStore,applyMiddleware} from "redux"
+import {createStore,applyMiddleware,compose} from "redux"
 import reducer from "../reducer/index"
 import thunk from "redux-thunk"
 
-const store = createStore(reducer,applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk)));
+    
 export default store;
