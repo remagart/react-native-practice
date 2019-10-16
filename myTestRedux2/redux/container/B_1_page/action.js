51c4cb2270abd3c64484fa5b0877daa105989054
remagart <jfmamjjasond418@gmail.com>
2019-10-16 11:58:28 +0800

    [Add] Redux建立-異步任務
    - 改成async/await
    - 加入錯誤處理的action
    
    20191016

--- myTestRedux2/redux/container/B_1_page/action.js
+++ myTestRedux2/redux/container/B_1_page/action.js
@@ -1,20 +1,41 @@
 import {REDUX_ACTION_TYPE} from "../../action/redux_action"
 
 const B1_PAGE_ACTION = {
-    fetchData: (data)=> ({type:REDUX_ACTION_TYPE.B1_PAGE_GET_DATA,payload:data})
+    fetchData: (data)=> ({type:REDUX_ACTION_TYPE.B1_PAGE_GET_DATA,payload:data}),
+    fetchData_fail: (error)=>({type: REDUX_ACTION_TYPE.B1_PAGE_GET_DATA_ERROR,error: error})
 }
 
-fetchList = () => {
-    return dispatch => {
-        return fetch("https://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json")
-        .then(res=>{
-            return res.json();
-        })
-        .then(data=>{
-            dispatch(B1_PAGE_ACTION.fetchData(data));
-        })
+// fetchList = () => {
+//     return dispatch => {
+//         return fetch("https://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json")
+//         .then(res=>{
+//             return res.json();
+//         })
+//         .then(data=>{
+//             dispatch(B1_PAGE_ACTION.fetchData(data));
+//         })
+//     }
+// }
+fetchList =  () => {
+    return async (dispatch) => {
+        try{
+            let url = "https://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json";
+            let response = await fetch(url,{method:"GET"});
+            let responseJSON = await response.json();
+            if(response.status == 200){
+                dispatch(B1_PAGE_ACTION.fetchData(responseJSON))
+                // return responseJSON;
+            }
+        }catch(err){
+            dispatch(B1_PAGE_ACTION.fetchData_fail(err.message))
+            console.log(typeof err);
+            console.log(err.message);
+            // return 
+        }
     }
 }
+
+
 
 module.exports = {
     B1_PAGE_ACTION: B1_PAGE_ACTION,
--- myTestRedux2/redux/container/B_1_page/reducer.js
+++ myTestRedux2/redux/container/B_1_page/reducer.js
@@ -2,12 +2,16 @@ import {REDUX_ACTION_TYPE} from "../../action/redux_action";
 
 const INIT_STATE = {
     lists: [],
+    loading: true,
+    err: "",
 }
 
 const B1_page_reducer = (state=INIT_STATE,action) => {
     switch(action.type){
         case REDUX_ACTION_TYPE.B1_PAGE_GET_DATA:
-            return {...state,lists: action.payload}
+            return {...state,lists: action.payload,loading: false}
+        case REDUX_ACTION_TYPE.B1_PAGE_GET_DATA_ERROR:
+            return {...state,loading: false,err: action.error}
         default:
             return state;
     }
