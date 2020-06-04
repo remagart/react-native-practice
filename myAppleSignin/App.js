d661dcc936c9f97ef961e5ed32023eb8b287a152
remagart <jfmamjjasond418@gmail.com>
2020-06-04 14:02:45 +0800

    點擊sign in apple

--- myAppleSignin/App.js
+++ myAppleSignin/App.js
@@ -8,7 +8,12 @@
 
 import React, { Component } from 'react';
 import { View, Text,StyleSheet,SafeAreaView } from 'react-native';
-import {AppleButton} from "@invertase/react-native-apple-authentication";
+import appleAuth,{
+  AppleButton,
+  AppleAuthRequestOperation,
+  AppleAuthRequestScope,
+  AppleAuthCredentialState,
+} from "@invertase/react-native-apple-authentication";
 import ToastComponent from "./component/ToastComponent";
 
 export default class App extends Component {
@@ -18,8 +23,21 @@ export default class App extends Component {
     };
   }
 
-  onAppleButtonPress = () => {
+  onAppleButtonPress = async () => {
     ToastComponent.showToast("click",ToastComponent.STATUS_TOAST.DEFAULT);
+    // performs login request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: AppleAuthRequestOperation.LOGIN,
+      requestedScopes: [AppleAuthRequestScope.EMAIL, AppleAuthRequestScope.FULL_NAME],
+    });
+
+    // get current authentication state for user
+    const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+
+    // use credentialState response to ensure the user is authenticated
+    if (credentialState === AppleAuthCredentialState.AUTHORIZED) {
+      // user is authenticated
+    }
   }
 
   render() {
